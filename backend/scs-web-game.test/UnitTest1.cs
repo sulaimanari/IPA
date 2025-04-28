e4174f7c92173f8a490e31bb516737c89a4db1db
Ari Sulaiman <ari.sulaiman@scs.ch>
2025-04-28 17:39:51 +0200

    Initial scs-web-game.test

---
+++ backend/scs-web-game.test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace scs_web_game.test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
