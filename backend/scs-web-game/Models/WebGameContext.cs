71c354cd99c35ab603ce6ef1ef72c03111751ae0
Ari Sulaiman <ari.sulaiman@scs.ch>
2025-04-28 10:20:39 +0200

    Initial Backend: scs-web-game Project

---
+++ backend/scs-web-game/Models/WebGameContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using scs_web_game.Models;
+namespace scs_web_game.Models;
+
+public partial class WebGameContext : DbContext
+{
+    public WebGameContext()
+    {
+    }
+
+    public WebGameContext(DbContextOptions<WebGameContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=BATANGA\\SQL22EXPRESS;Database=scs-web-game; Integrated Security=true;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
