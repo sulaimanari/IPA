e8b14608d6e42360776748ea4b7c6084c65bf3f9
Ari Sulaiman <ari.sulaiman@scs.ch>
2025-05-02 13:07:48 +0200

    Migrate tables to the database

--- backend/scs-web-game/Migrations/20250502084342_InitalTables.Designer.cs
+++ backend/scs-web-game/Migrations/20250502084342_InitalTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using scs_web_game.Models;
 
@@ -11,9 +12,11 @@ using scs_web_game.Models;
 namespace scs_web_game.Migrations
 {
     [DbContext(typeof(WebGameContext))]
-    partial class WebGameContextModelSnapshot : ModelSnapshot
+    [Migration("20250502084342_InitalTables")]
+    partial class InitalTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/scs-web-game/Migrations/20250502084342_InitalTables.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace scs_web_game.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitalTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    UserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ImgFileName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Role = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    PlayerEmail = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false, collation: "SQL_Latin1_General_CP1_CI_AS"),
+                    PlayerName = table.Column<string>(type: "nvarchar(95)", maxLength: 95, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "(newid())"),
+                    Score = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    PlayerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.ID);
+                    table.ForeignKey(
+                        name: "ForeignKey_Game_Player",
+                        column: x => x.PlayerId,
+                        principalTable: "Player",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_PlayerId",
+                table: "Game",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Player_PlayerEmail",
+                table: "Player",
+                column: "PlayerEmail",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
