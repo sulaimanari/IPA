63b9c267d8b2d5379dfc134df743ad3b5feb7553
Ari Sulaiman <ari.sulaiman@scs.ch>
2025-05-02 15:58:58 +0200

    Create Player Data Access Layer and Player Dto

---
+++ backend/scs-web-game/DTOs/Player/PlayerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace scs_web_game.DTOs.Player
+{
+    public class PlayerDto
+    {
+        public Guid PlayerId { get; set; }
+        public string PlayerEmail { get; set; } = null!;
+        public string PlayerName { get; set; } = null!;
+
+    }
+}
---
+++ backend/scs-web-game/DataAccessLayer/PlayerDal.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using scs_web_game.DTOs.Player;
+using scs_web_game.Models;
+using ILogger = Serilog.ILogger;
+namespace scs_web_game.DataAccessLayer
+{
+    public class PlayerDal(WebGameContext context, ILogger logger)
+    {
+        public async Task<PlayerDto> CreatePlayer(string email)
+        {
+            var newPlayer = new Player
+            {
+                PlayerEmail = email
+            };
+            var playerName = ExtractPlayerNameFromEmail(email);
+            context.Player.Add(newPlayer);
+            await context.SaveChangesAsync();
+
+            logger.Information("Player created successfully with ID: {PlayerId}", newPlayer.PlayerId);
+
+            return new PlayerDto
+            {
+                PlayerId = newPlayer.PlayerId,
+                PlayerEmail = newPlayer.PlayerEmail,
+                PlayerName = playerName
+            };
+        }
+        private string ExtractPlayerNameFromEmail(string email)
+        {
+            var atIndex = email.IndexOf("@scs.ch", StringComparison.OrdinalIgnoreCase);
+            return email[..atIndex];
+            logger.Information("Extracted player name: {PlayerName}", email[..atIndex]);
+        }
+
+        public async Task<PlayerDto> GetPlayerIdByEmail(string email)
+        {
+            var player = await context.Player
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.PlayerEmail == email);
+
+            logger.Information("Player found with ID: {PlayerId}", player.PlayerId);
+
+            return new PlayerDto
+            {
+                PlayerId = player.PlayerId,
+                PlayerEmail = player.PlayerEmail,
+                PlayerName = player.PlayerName
+            };
+        }
+
+        public async Task<List<PlayerDto>> GetALlPlayers()
+        {
+            var players = await context.Player
+                .AsNoTracking()
+                .ToListAsync();
+
+            logger.Information("Retrieved {Count} players from the database.", players.Count);
+
+            return players.Select(player => new PlayerDto
+            {
+                PlayerId = player.PlayerId,
+                PlayerEmail = player.PlayerEmail,
+                PlayerName = player.PlayerName
+            }).ToList();
+        }
+    }
+}
