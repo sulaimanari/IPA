aaef4826006c6a5f4aa6434b513be63dfa7b60ac
Ari Sulaiman <ari.sulaiman@scs.ch>
2025-05-07 16:18:15 +0200

    edit app module and app routing module

--- frontend/scs-web-game-frontend/src/app/app-routing.module.ts
+++ frontend/scs-web-game-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TimerComponent } from './components/timer/timer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HighScoreComponent } from './components/high-score/high-score.component';
+import { PlayComponent } from './components/play/play.component';
+import { IdentifyComponent } from './components/identify/identify.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'identify', pathMatch: 'full' },
+  { path: 'identify', component: IdentifyComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'high-score', component: HighScoreComponent },
+  { path: 'play', component: PlayComponent },
+  { path: '**', redirectTo: 'identify' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/scs-web-game-frontend/src/app/app.module.ts
+++ frontend/scs-web-game-frontend/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlayComponent } from './components/play/play.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { IdentifyComponent } from './components/identify/identify.component';
+import { HighScoreComponent } from './components/high-score/high-score.component';
+import { CoreApiModule } from './core/core-api.module';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayComponent,
+    TimerComponent,
+    IdentifyComponent,
+    HighScoreComponent,
+    HeaderComponent,
   ],
   imports: [
+    CoreApiModule,
+    CommonModule,
+    FormsModule, 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
